7593b6b404e66b1bade8ac8b87650df354fe6661
Shaurya9935 <prakhargupta9935@gmail.com>
2026-08-05 15:41:05 +0530

    feat: enhance database connection handling and improve error logging

--- server/src/common/config/db.js
+++ server/src/common/config/db.js
@@ -7,20 +7,29 @@ import * as schema from "../../db/schema.js";
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined");
 }
+console.log(process.env.DATABASE_URL?.replace(/:[^:@]+@/, ":****@"));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const db = drizzle(pool, { schema });
 
 const connectDB = async () => {
   try {
-    await db.execute(sql`SELECT 1`);
+    const client = await pool.connect();
+
+    await client.query("SELECT 1");
+
+    client.release();
+
     console.log("PostgreSQL connected Successfully");
-  } catch (error) {
-    console.error(" Database connection failed");
-    throw error;
+  } catch (err) {
+    console.error(err);
+    throw err;
   }
 };
 
